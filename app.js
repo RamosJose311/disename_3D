803cc7a3fcfff764682f7cfb6ac4b16d9877717b
GeremiasZalokar <zalokar.16gere@gmail.com>
2022-07-16 21:38:23 -0300

    inicializando servidor, Sprint 2

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const serv = 3030;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'home.html' )));
+
+app.listen(serv, () => console.log('server init in ' + serv));
