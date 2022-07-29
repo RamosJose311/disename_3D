5128c4ab87db1dc31e82974de0a8f3c75ca41f5d
santysrodriguez <santiagocarp987@gmail.com>
2022-07-29 17:32:56 -0300

    detalle terminado

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'home.html' )));
 
 app.listen(serv, () => console.log('Server runing in http://localhost:' + serv));
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/detalle', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'detalle.html' )));
+
