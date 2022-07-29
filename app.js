3dfa4f0857f32ba6a209493b3690aa1b56afb9bf
JuanLicciardi <juanelicciardi@gmail.com>
2022-07-29 01:20:03 -0300

    Iniciando maquetacion Carrito de Compras

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const serv = 3030;
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'home.html' )));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname ,'view','home.html')));
+app.get('/productCart.html', (req, res) => res.sendFile(path.resolve(__dirname ,'view','productCart.html' )));
+
+
 
 app.listen(serv, () => console.log('Server runing in http://localhost:' + serv));
