697fc4de96223b6176c106c6816d5ec438320939
JuanLicciardi <juanelicciardi@gmail.com>
2022-08-01 00:01:26 -0300

    Solucionadndo Conflicto en Develop // Servidor funcionando

--- app.js
+++ app.js
@@ -6,32 +6,14 @@ const serv = 3030;
 app.use(express.static('public'));
 
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname ,'view','home.html')));
+
 app.get('/productCart.html', (req, res) => res.sendFile(path.resolve(__dirname ,'view','productCart.html' )));
-
-
-
-app.listen(serv, () => console.log('Server runing in http://localhost:' + serv));
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.get('/detalle', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'detalle.html' )));
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'inicio.html' )));
 
-<<<<<<< HEAD
+
+
 app.listen(serv, () => console.log('Server runing in http://localhost:' + serv));
 
 
 
-app.get('/inicio', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'inicio.html' )));
-=======
->>>>>>> 8432ea0d4b9e53ee00daa180aa5bd5a8d2f85094
