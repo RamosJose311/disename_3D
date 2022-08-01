d77ae57f343868acd58510f641a1573235b4ff39
JuanLicciardi <juanelicciardi@gmail.com>
2022-08-01 04:33:35 -0300

    Formulario Register finalizado

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const serv = 3030;
 app.use(express.static('public'));
 
 
-
-app.get('/productCart.html', (req, res) => res.sendFile(path.resolve(__dirname ,'view','productCart.html' )));
-app.get('/detalle', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'detalle.html' )));
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'inicio.html' )));
+app.get('/productCart.html', (req, res) => res.sendFile(path.resolve(__dirname ,'view','productCart.html' )));
+app.get('/detalle,html', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'detalle.html' )));
+app.get('/login.html', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'login.html' )));
+app.get('/register.html', (req, res) => res.sendFile(path.resolve(__dirname , 'view', 'register.html' )));
+
 
 
 
