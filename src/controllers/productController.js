16f05e6748b2dfe57672e9bb213fa66ceb258a06
RamosJose311 <joseezequiel01@hotmail.es>
2022-08-16 23:44:21 -0300

    agregando nuevas viñetas de compra

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,6 +5,14 @@ module.exports = {
     productCart : (req,res) =>{
         return res.render('productCart')
     },
-    
+    archivo :(req,res) =>{
+        return res.render('archivo')
+    },
+    personalizado:(req,res) =>{
+        return res.render('personalizado')
+    },
+    proyecto :(req,res) =>{
+        return res.render('proyecto')
+    }
 
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const {detalle,productCart,crearProducto,editarProducto} = require('../controllers/productController');
+const {detalle,productCart,archivo,personalizado,proyecto,} = require('../controllers/productController');
 
 /* /products*/
 router
     .get('/detalle', detalle)
     .get('/productCart', productCart)
-
+    .get('/archivo',archivo)
+    .get('/personalizado',personalizado)
+    .get('/proyecto',proyecto)
 module.exports = router;
