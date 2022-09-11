5f4f4fdacc3d9741d6def26b1d3a0cfc24a88835
Ramosjose311 <joseezequiel01@hotmail.es>
2022-09-11 19:38:11 -0300

    arreglos del formulario

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,5 @@
+const {loadUser} =require ("../data/dbModules")
+
 module.exports = {
     login : (req,res) =>{
         return res.render('login')
@@ -6,6 +8,25 @@ module.exports = {
         return res.render('register')
     },
     processRegister :(req,res) =>{
-        return res.send (req.body)
+        const {nombre,apellido,email,password,} =req.body;
+        const usuario=loadUser;
+
+        const nuevoUsuario={
+            Id:usuario[usuario.length=1] ? usuario[usuario.length=1].id+1:1,
+            nombre:nombre.trim(),
+            apellido:apellido.trim(),
+            email:email.trim(),
+            password ,
+            Rol:"user",
+            avatar:null,
+
+
+        }
+
+        const userModify=[...usuario,nuevoUsuario];
+
+        storeUser(userModify);
+        return res.redirec('/users/login')
     }
+    
  }
