d39b97d198af47a734027d742fb92b51fd338339
Ramosjose311 <joseezequiel01@hotmail.es>
2022-11-18 02:44:37 -0300

    avanze en api usuario

--- src/controllers/api/apiUsersController.js
+++ src/controllers/api/apiUsersController.js
@@ -1,9 +1,52 @@
-
+const db=require('../../database/models')
+const {literal}=require('sequelize')
 
 
 module.exports = {
     all : async (req,res) => {
-        /* devuelve todos los usuarios */
+        try { 
+            let options={ attributes: {
+                exclude:["createdAt","updatedAt"],
+                include:[[literal(`CONCAT('${req.protocol}://${req.get('host')}/api/user/`,'userId)'),'userURL']]
+            },
+            include:[
+            {
+                association:'avatars',
+                attributes:{
+                    exclude:["createdAt","updatedAt"],
+                   
+                }
+            }
+         ]
+        
+    }
+            /*let users = await db.User.findAll(options)*/
+            let {count,rows}=await db.User.findAndCountAll(options)
+
+            return res.status(200).json({
+                ok: true,
+                meta: {
+                    status: 200,
+                    total: count
+                },
+                data: {
+                    users: rows
+                }
+            })
+            
+          
+
+        } catch (error) {
+            console.log(error);
+            return res.status(error.status || 500).json({
+                ok: false,
+                msg: error.message,
+            });
+            
+        }
+
+        
+        
     },
     getOne : async (req,res) => {
         /* devuelve solo un usuario */
