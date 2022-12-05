4ebebac014a0bf7aade0b81ca2091b48395ec180
Ramosjose311 <joseezequiel01@hotmail.es>
2022-12-05 19:41:51 -0300

    validacion front login

---
+++ public/javascripts/userLogin.js
@@ -0,0 +1,48 @@
+const $ = (element) => document.getElementById(element);
+
+
+const exRegs = {
+    exRegAlfa: /^[A-Za-zÁÉÍÓÚáéíóúñÑ ]+$/,
+    exRegEmail: /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/ /* /w+([.-]?\w+)*(\.\w{2,10})+$ / */,
+    exRegPass:
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&]{6,12}/,}
+
+$("email").addEventListener("blur",async function(){
+    switch (true) {
+        case !this.value.trim():
+            $('errorEmail').innerText = 'El email es obligatorio'
+            break;
+    
+        case !exRegs.exRegEmail.test(this.value):
+            $('errorEmail').innerText ="El Email no tiene el formato correcto"
+            break;
+
+         default:
+            $('errorEmail').innerText =null
+            break;
+    }
+}),
+$("password").addEventListener("blur",function(){
+    switch (true) {
+        case !this.value.trim():
+            $('errorPassword').innerText = 'La contraseña es obligatoria'
+            break;
+
+
+        default:
+            $('errorPassword').innerText =null
+            break;
+    }
+})
+$("register_section_main-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const elements = this.elements;
+        for (let i = 0; i < elements.length - 1; i++) {
+
+        if(!elements[i].value.trim()){
+            $('errorForm').innerText = 'Algunos de los campos no es valido'
+        }
+    }
+
+});
