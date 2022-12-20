1d9882f8b82bde6d7a9edc6b705bb02488bad0f0
JuanLicciardi <juanelicciardi@gmail.com>
2022-12-20 18:10:03 -0300

    Actualizando

--- admin/src/components/contentWrapper/Categorias/CategoriesInDb.js
+++ admin/src/components/contentWrapper/Categorias/CategoriesInDb.js
@@ -6,6 +6,37 @@ import WarapperCategories from './WarapperCategories';
 
 function CategoriesInDb() {
 
+      const [products, setproducts] = useState({
+        data:[]
+    })
+
+    useEffect( () =>  {
+       const ApiCall = async (url) => {
+                const resp = await fetch(url)
+                const data = await resp.json()
+                console.log("Lo podre ver",data.data.productos)
+                {
+                    if(data.data.productos.length === 0){
+                        setproducts({
+                            ...products,
+                            data: data.data.productos
+                        })
+                    } else {
+                        setproducts({
+                            ...products,
+                            data: data.data.productos
+                        })
+                        console.log("esto estoy viendo",products)
+
+                        
+                    }
+                  }
+
+            }
+    
+        ApiCall ("http://localhost:4000/api/products/")
+
+    }, [])
 
     const [categories, setCategories] = useState({
         data:[]
@@ -34,46 +65,13 @@ function CategoriesInDb() {
         
     },[])
 
-
-    const [products, setproducts] = useState({
-        data:[]
-    })
-
-    useEffect( () =>  {
-       const ApiCall = async (url) => {
-                const resp = await fetch(url)
-                const data = await resp.json()
-                console.log("Lo podre ver",data.data.productos)
-                {
-                    if(data.data.productos.length === 0){
-                        setproducts({
-                            ...products,
-                            data: data.data.productos
-                        })
-                    } else {
-                        setproducts({
-                            ...products,
-                            data: data.data.productos
-                        })
-                        console.log("esto estoy viendo",products)
-
-                    }
-                  }
-                  
-            }
-    
-        ApiCall ("http://localhost:4000/api/products/")
-
-    }, [])
-
-
-
-
-    
+   
+const aux1 = categories.data.length
     
   return (
     <React.Fragment>
         {categories.data.length === 0 && <p>Cargando....</p>}
+        {console.log("Que pasa con aux1",aux1)}
         {
            categories.data.map((elem,index)=>{
             console.log("que mostras: ",elem);
